9abc2c0e43b691644fbe941f2e3ee739ec3d8345
Taher <Taher@192.168.1.6>
2018-12-29 19:48:10 +0530

    FirstCommit

---
+++ _3AutomaticallyWiringBeans/src/main/java/com/spring/_3AutomaticallyWiringBeans/App.java
@@ -0,0 +1,13 @@
+package com.spring._3AutomaticallyWiringBeans;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
